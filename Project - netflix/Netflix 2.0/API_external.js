c6112e7d26c793d0d36bbabe5ae16d8002a497e8
FredLau97 <frederik-lau@live.dk>
2020-04-30 14:50:21 +0200

    Tilføjet genre arrays, samt en funktion til automatisk at tilføje filmene til de rigtige genre arrays

--- Project - netflix/Netflix 2.0/API_external.js
+++ Project - netflix/Netflix 2.0/API_external.js
@@ -1,8 +1,27 @@
-// var genreArr = [];
 var movieIDArr = [];
 var TD = [];
 var movies = [];
 var url = "";
+var temp = [];
+
+// Arrays til de forskellige genre
+var documentary = [];
+var drama = [];
+var action = [];
+var comedy = [];
+var romance = [];
+var thriller = [];
+var crime = [];
+var scifi = [];
+var anim = [];
+var family = [];
+var short = [];
+var news = [];
+var sport = [];
+var adventure = [];
+var horror = [];
+var musical = [];
+var buffer = [];
 
 // Hent film ID fra API
 function GetMovies(callback) {
@@ -49,7 +68,65 @@ function GetTitleDetails(callback) {
 
 // Læs JSON fil med filmobjekter, og tilføj dem til movies (array)
 $.getJSON("movies.json", function(response) {
-    for(var i = 0; i < response.length; i++) {
-        movies.push(response[i]);
-    }
+    movies.push(response);
+    
+    AssignMovieToGenre();
+    
 });
+
+function AssignMovieToGenre() {
+    for (var i = 0; i < movies[0].length; i++) {
+        if (movies[0][i].genre.includes('Documentary'))
+            documentary.push(JSON.stringify(movies[0][i]));
+        if (movies[0][i].genre.includes("Drama"))
+            drama.push(JSON.stringify(movies[0][i]));
+        if (movies[0][i].genre.includes("Action"))
+            action.push(JSON.stringify(movies[0][i]));
+        if (movies[0][i].genre.includes("Comedy"))
+            comedy.push(JSON.stringify(movies[0][i]));
+        if (movies[0][i].genre.includes("Romance"))
+            romance.push(JSON.stringify(movies[0][i]));
+        if (movies[0][i].genre.includes("Thriller"))
+            thriller.push(JSON.stringify(movies[0][i]));
+        if (movies[0][i].genre.includes("Crime"))
+            crime.push(JSON.stringify(movies[0][i]));
+        if (movies[0][i].genre.includes("Sci-Fi"))
+            scifi.push(JSON.stringify(movies[0][i]));
+        if (movies[0][i].genre.includes("Animation"))
+            anim.push(JSON.stringify(movies[0][i]));
+        if (movies[0][i].genre.includes("Family"))
+            family.push(JSON.stringify(movies[0][i]));
+        if (movies[0][i].genre.includes("Short"))
+            short.push(JSON.stringify(movies[0][i]));
+        if (movies[0][i].genre.includes("News"))
+            news.push(JSON.stringify(movies[0][i]));
+        if (movies[0][i].genre.includes("Sport"))
+            sport.push(JSON.stringify(movies[0][i]));
+        if (movies[0][i].genre.includes("Adventure"))
+            adventure.push(JSON.stringify(movies[0][i]));
+        if (movies[0][i].genre.includes("Horror"))
+            horror.push(JSON.stringify(movies[0][i]));
+        if (movies[0][i].genre.includes("Musical"))
+            musical.push(JSON.stringify(movies[0][i]));
+        if (movies[0][i].genre == "null")
+            buffer.push(JSON.stringify(movies[0][i]));
+    }
+    
+    console.log("Documentary:" + documentary);
+    console.log("Drama:" + drama);
+    console.log("Action:" + action);
+    console.log("Comedy:" + comedy);
+    console.log("Romance:" + romance);
+    console.log("Thriller:" + thriller);
+    console.log("Crime:" + crime);
+    console.log("Sci-Fi:" + scifi);
+    console.log("Animation:" + anim);
+    console.log("Family:" + family);
+    console.log("Short:" + short);
+    console.log("News:" + news);
+    console.log("Sport:" + sport);
+    console.log("Adventure:" + adventure);
+    console.log("Horror:" + horror);
+    console.log("Musical:" + musical);
+    console.log("Buffer:" + buffer);
+}
