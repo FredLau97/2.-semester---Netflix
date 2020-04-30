6932b1a77ace47e3ab8492c3ce299b8b884bd5f5
FredLau97 <frederik-lau@live.dk>
2020-04-30 12:18:36 +0200

    API kald færdige + JSOn fil med film objekter

--- Project - netflix/Netflix 2.0/API_external.js
+++ Project - netflix/Netflix 2.0/API_external.js
@@ -1,11 +1,17 @@
-// JavaScript Document
-function GetGenres(callback) {
+// var genreArr = [];
+var movieIDArr = [];
+var TD = [];
+var movies = [];
+var url = "";
+
+// Hent film ID fra API
+function GetMovies(callback) {
     var settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://unogsng.p.rapidapi.com/genres",
+	"url": "https://unogsng.p.rapidapi.com/search?type=movie&limit=100&countrylist=78",
 	"method": "GET",
-    "dataTyoe": "string",
+    "success": "callback",
 	"headers": {
 		"x-rapidapi-host": "unogsng.p.rapidapi.com",
 		"x-rapidapi-key": "dc673d39ecmsha1be03513d10792p1fb782jsn1d5649ce19e6"
@@ -14,12 +20,36 @@ function GetGenres(callback) {
 
     
 $.ajax(settings).done(function (response) {
-    console.log(response.results)
-    
-    for(var i = 0; i < response.results.length; i++){
-        PLEASELIST.push(response.results[i]);
+    for(var i = 0; i < response.results.length; i++) {
+        movieIDArr.push(response.results[i].nfid);
     }
-
-    success: callback
+    GetTitleDetails(callback);
 });
 }
+
+// Få detaljeret film objekter fra API
+function GetTitleDetails(callback) {
+    for (var i = 0; i < movieIDArr.length; i++) {
+        url = "https://unogsng.p.rapidapi.com/title?netflixid="
+        url += movieIDArr[i].toString();
+        var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": url,
+	"method": "GET",
+    "success": "callback",
+	"headers": {
+		"x-rapidapi-host": "unogsng.p.rapidapi.com",
+		"x-rapidapi-key": "dc673d39ecmsha1be03513d10792p1fb782jsn1d5649ce19e6"
+	}
+}
+
+};
+}
+
+// Læs JSON fil med filmobjekter, og tilføj dem til movies (array)
+$.getJSON("movies.json", function(response) {
+    for(var i = 0; i < response.length; i++) {
+        movies.push(response[i]);
+    }
+});
