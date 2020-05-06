02410cb7a1f4d5273d5d8a38931b4d7a8c07523c
FredLau97 <frederik-lau@live.dk>
2020-05-06 17:52:02 +0200

    blah

--- Project - netflix/Netflix 2.0/main.js
+++ Project - netflix/Netflix 2.0/main.js
@@ -25,11 +25,7 @@ function GenerateMatch() {
     $("#title").prepend(`<h1 class = "whiteText movTitle">${action[rndMovieInd].title}</h1>`);
     $('.movRating').prepend(`<strong>IMDB</strong>: ${action[rndMovieInd].rating} / 10`);
     $('.synopsis').append(`${action[rndMovieInd].synopsis}`);
-<<<<<<< HEAD
     $('#poster').prepend(`<img class="col-12 movPoster" src="${action[rndMovieInd].img}" style="width: 600px;">`);
     $('#year').prepend(`<p><strong>Årstal:</strong> ${action[rndMovieInd].year}</p>`);
-=======
     $('#poster').prepend(`<img class="col-12 movPoster" src="${action[rndMovieInd].img}">`);
-    
->>>>>>> master
 }
