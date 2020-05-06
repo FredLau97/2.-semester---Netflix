cfdfb331f992ddcb4f4581ae614294a5eaf84cd5
FredLau97 <frederik-lau@live.dk>
2020-05-06 16:15:50 +0200

    Nye genrer på forsiden

--- Project - netflix/Netflix 2.0/main.js
+++ Project - netflix/Netflix 2.0/main.js
@@ -1,17 +1,17 @@
 function AddMovies() {
     let actionMovies = $(`.action`);
-    let dramaMovies = $(`.drama`);
-    let comedyMovies = $(`.comedy`);
+    let docMovies = $(`.doc`);
+    let thrillerMovies = $(`.thriller`);
     
     for(var i = 0; i < actionMovies.length; i++) {
         $(actionMovies[i]).prepend(`<a href=""><img class="moviePoster m-x-auto d-none d-sm-block" src="${action[i].img}" alt=""></a>`);
     }
     
-    for(var i = 0; i < dramaMovies.length; i++) {
-        $(dramaMovies[i]).prepend(`<a href=""><img class="moviePoster m-x-auto d-none d-sm-block" src="${drama[i].img}" alt=""></a>`);
+    for(var i = 0; i < docMovies.length; i++) {
+        $(docMovies[i]).prepend(`<a href=""><img class="moviePoster m-x-auto d-none d-sm-block" src="${documentary[i].img}" alt=""></a>`);
     }
     
-    for(var i = 0; i < comedyMovies.length; i++) {
-        $(comedyMovies[i]).prepend(`<a href=""><img class="moviePoster m-x-auto d-none d-sm-block" src="${comedy[i].img}" alt=""></a>`);
+    for(var i = 0; i < thrillerMovies.length; i++) {
+        $(thrillerMovies[i]).prepend(`<a href=""><img class="moviePoster m-x-auto d-none d-sm-block" src="${thriller[i].img}" alt=""></a>`);
     }
 }
