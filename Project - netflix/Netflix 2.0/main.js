3a81ced63ad5e187348d5d6c989d3a599637ccfd
FredLau97 <frederik-lau@live.dk>
2020-05-06 16:44:51 +0200

    Rettelser

--- Project - netflix/Netflix 2.0/main.js
+++ Project - netflix/Netflix 2.0/main.js
@@ -2,16 +2,19 @@ function AddMovies() {
     let actionMovies = $(`.action`);
     let docMovies = $(`.doc`);
     let thrillerMovies = $(`.thriller`);
+    let actionData = action.slice(0, actionMovies.length);
+    let docData = documentary.slice(0, docMovies.length);
+    let thrillerData = thriller.slice(0, thrillerMovies.length);
     
     for(var i = 0; i < actionMovies.length; i++) {
-        $(actionMovies[i]).prepend(`<a href=""><img class="moviePoster m-x-auto d-none d-sm-block" src="${action[i].img}" alt=""></a>`);
+        $(actionMovies[i]).prepend(`<a href=""><img class="moviePoster m-x-auto d-none d-sm-block actM${i}" src="${actionData[i].img}" alt=""></a>`);
     }
     
     for(var i = 0; i < docMovies.length; i++) {
-        $(docMovies[i]).prepend(`<a href=""><img class="moviePoster m-x-auto d-none d-sm-block" src="${documentary[i].img}" alt=""></a>`);
+        $(docMovies[i]).prepend(`<a href=""><img class="moviePoster m-x-auto d-none d-sm-block docM${i}" src="${docData[i].img}" alt=""></a>`);
     }
     
     for(var i = 0; i < thrillerMovies.length; i++) {
-        $(thrillerMovies[i]).prepend(`<a href=""><img class="moviePoster m-x-auto d-none d-sm-block" src="${thriller[i].img}" alt=""></a>`);
+        $(thrillerMovies[i]).prepend(`<a href=""><img class="moviePoster m-x-auto d-none d-sm-block thrlM${i}" src="${thrillerData[i].img}" alt=""></a>`);
     }
 }
