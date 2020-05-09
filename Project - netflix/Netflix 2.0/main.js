571bc1c1a4bf1b96d82571c2595bb5f390ca0cf0
FredLau97 <frederik-lau@live.dk>
2020-05-09 15:25:28 +0200

    Lille fix

--- Project - netflix/Netflix 2.0/main.js
+++ Project - netflix/Netflix 2.0/main.js
@@ -9,107 +9,19 @@ function AddMovies() {
     let watchData = buffer.slice(0, watchMovies.length);
     
     for(var i = 0; i < watchMovies.length; i++) {
-        $(watchMovies[i]).prepend(`                <button type="button" class="modal-button myBtn">
-                    <img class="moviePoster m-x-auto d-none d-sm-block actM${i}" src="${watchData[i].img}" alt="">
-                </button>
-                <div class="modal fade myModal" role="dialog">
-                    <div class="modal-dialog">
-
-                        <!-- Modal content-->
-                        <div class="modal-content modal-body">
-                            <div class="modal-header ">
-                                <h4 class="modal-title">${watchData[i].title}</h4>
-                                <button type="button" class="close modal-body" data-dismiss="modal">&times;</button>
-                            </div>
-                            <div class="modal-body">
-                                <img class="moviePoster m-x-auto d-none d-sm-block actM${i}" src="${watchData[i].img}" alt="">
-                                <p class="actM${i}">${watchData[i].synopsis}</p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-danger" data-dismiss="modal">Play</button>
-                                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>`);
+        $(watchMovies[i]).prepend();
     }
 
     for(var i = 0; i<actionMovies.length; i++){
-        $(actionMovies[i]).prepend(`                <button type="button" class="modal-button myBtn">
-                    <img class="moviePoster m-x-auto d-none d-sm-block actM${i}" src="${actionData[i].img}" alt="">
-                </button>
-                <div class="modal fade myModal" role="dialog">
-                    <div class="modal-dialog">
-
-                        <!-- Modal content-->
-                        <div class="modal-content modal-body">
-                            <div class="modal-header ">
-                                <h4 class="modal-title">${actionData[i].title}</h4>
-                                <button type="button" class="close modal-body" data-dismiss="modal">&times;</button>
-                            </div>
-                            <div class="modal-body">
-                                <img class="moviePoster m-x-auto d-none d-sm-block actM${i}" src="${actionData[i].img}" alt="">
-                                <p class="actM${i}">${actionData[i].synopsis}</p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-danger" data-dismiss="modal">Play</button>
-                                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>`);
+        $(actionMovies[i]).prepend();
     }
     
     for(var i = 0; i < docMovies.length; i++) {
-        $(docMovies[i]).prepend(`                <button type="button" class="modal-button myBtn">
-                    <img class="moviePoster m-x-auto d-none d-sm-block actM${i}" src="${docData[i].img}" alt="">
-                </button>
-                <div class="modal fade myModal" role="dialog">
-                    <div class="modal-dialog">
-
-                        <!-- Modal content-->
-                        <div class="modal-content modal-body">
-                            <div class="modal-header ">
-                                <h4 class="modal-title">${docData[i].title}</h4>
-                                <button type="button" class="close modal-body" data-dismiss="modal">&times;</button>
-                            </div>
-                            <div class="modal-body">
-                                <img class="moviePoster m-x-auto d-none d-sm-block actM${i}" src="${docData[i].img}" alt="">
-                                <p class="actM${i}">${docData[i].synopsis}</p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-danger" data-dismiss="modal">Play</button>
-                                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>`);
+        $(docMovies[i]).prepend();
     }
     
     for(var i = 0; i < thrillerMovies.length; i++) {
-        $(thrillerMovies[i]).prepend(`                <button type="button" class="modal-button myBtn">
-                    <img class="moviePoster m-x-auto d-none d-sm-block actM${i}" src="${thrillerData[i].img}" alt="">
-                </button>
-                <div class="modal fade myModal" role="dialog">
-                    <div class="modal-dialog">
-
-                        <!-- Modal content-->
-                        <div class="modal-content modal-body">
-                            <div class="modal-header ">
-                                <h4 class="modal-title">${thrillerData[i].title}</h4>
-                                <button type="button" class="close modal-body" data-dismiss="modal">&times;</button>
-                            </div>
-                            <div class="modal-body">
-                                <img class="moviePoster m-x-auto d-none d-sm-block actM${i}" src="${thrillerData[i].img}" alt="">
-                                <p class="actM${i}">${thrillerData[i].synopsis}</p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-danger" data-dismiss="modal">Play</button>
-                                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>`);
+        $(thrillerMovies[i]).prepend();
     }
 }
 
